d95e8fc147d6fea694e3a7dbe02d83c5c9f7126a
Musa Daniel <jtccontractsservicesng@yahoo.com>
2024-05-22 00:14:11 +0100

    Initial commit

---
+++ src/main/java/com/jtc/microservice/authorization/server/MicroserviceAuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package com.jtc.microservice.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicroserviceAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtc/microservice/authorization/server/MicroserviceAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtc.microservice.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
